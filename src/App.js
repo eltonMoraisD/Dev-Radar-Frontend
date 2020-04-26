5c46d06544f2cc08c336b65a3198c143d479f073
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-04-26 20:57:19 -0100

    estilizando a pagina

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
 import React from 'react';
-import logo from './logo.svg';
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="app">
+			<aside>
+				<strong>Cadastrar</strong>
+
+				<form>
+					<div className="input-block">
+						<label htmlFor="github_username">Usuario do Github</label>
+						<input name="github_username" id="github_username" required />
+					</div>
+
+					<div className="input-block">
+						<label htmlFor="techs">Tecnologias</label>
+						<input name="techs" id="techs" required />
+					</div>
+
+					<div className="input-group">
+						<div className="input-block">
+							<label htmlFor="latitude">Latitude</label>
+							<input name="latitude" id="latitude" required />
+						</div>
+
+						<div className="input-block">
+							<label htmlFor="longitude">Longitude</label>
+							<input name="longitude" id="longitude" required />
+						</div>
+					</div>
+					<button type="submit">Salvar</button>
+				</form>
+			</aside>
+
+			<main>
+				<ul>
+					<li className="dev-item">
+						<header>
+							<img
+								src="https://avatars2.githubusercontent.com/u/39710764?s=460&u=7912ae5b1093e79f24c6f42bee54951a313d551d&v=4"
+								alt="avatar"
+							/>
+							<div className="user-info">
+								<strong>Elton Morais</strong>
+								<span>ReactJS, React Native, Node.js</span>
+							</div>
+						</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin pulvinar
+							sodales.
+						</p>
+						<a href="https://github.com/eltonMoraisD">Acessar perfil no github</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img
+								src="https://avatars2.githubusercontent.com/u/39710764?s=460&u=7912ae5b1093e79f24c6f42bee54951a313d551d&v=4"
+								alt="avatar"
+							/>
+							<div className="user-info">
+								<strong>Elton Morais</strong>
+								<span>ReactJS, React Native, Node.js</span>
+							</div>
+						</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin pulvinar
+							sodales.
+						</p>
+						<a href="https://github.com/eltonMoraisD">Acessar perfil no github</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img
+								src="https://avatars2.githubusercontent.com/u/39710764?s=460&u=7912ae5b1093e79f24c6f42bee54951a313d551d&v=4"
+								alt="avatar"
+							/>
+							<div className="user-info">
+								<strong>Elton Morais</strong>
+								<span>ReactJS, React Native, Node.js</span>
+							</div>
+						</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin pulvinar
+							sodales.
+						</p>
+						<a href="https://github.com/eltonMoraisD">Acessar perfil no github</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img
+								src="https://avatars2.githubusercontent.com/u/39710764?s=460&u=7912ae5b1093e79f24c6f42bee54951a313d551d&v=4"
+								alt="avatar"
+							/>
+							<div className="user-info">
+								<strong>Elton Morais</strong>
+								<span>ReactJS, React Native, Node.js</span>
+							</div>
+						</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sollicitudin pulvinar
+							sodales.
+						</p>
+						<a href="https://github.com/eltonMoraisD">Acessar perfil no github</a>
+					</li>
+				</ul>
+			</main>
+		</div>
+	);
 }
 
 export default App;
