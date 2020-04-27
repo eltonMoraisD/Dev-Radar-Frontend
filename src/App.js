41e24f1aba652d69f37fd824316dedcbb7f17dde
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-04-26 23:42:52 -0100

    adicionar responsividade e pegando a geolocalizaçao do usuario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
+	const [ latitude, setLatitude ] = useState('');
+	const [ longitude, setLongitude ] = useState('');
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const { latitude, longitude } = position.coords;
+
+				setLatitude(latitude);
+				setLongitude(longitude);
+			},
+			(err) => {
+				console.log(err);
+			},
+			{
+				timeout: 30000
+			}
+		);
+	}, []);
+
 	return (
 		<div id="app">
 			<aside>
@@ -24,12 +44,26 @@ function App() {
 					<div className="input-group">
 						<div className="input-block">
 							<label htmlFor="latitude">Latitude</label>
-							<input name="latitude" id="latitude" required />
+							<input
+								type="number"
+								name="latitude"
+								id="latitude"
+								required
+								value={latitude}
+								onChange={(e) => setLatitude(e.target.value)}
+							/>
 						</div>
 
 						<div className="input-block">
 							<label htmlFor="longitude">Longitude</label>
-							<input name="longitude" id="longitude" required />
+							<input
+								type="number"
+								name="longitude"
+								id="longitude"
+								required
+								value={longitude}
+								onChange={(e) => setLongitude(e.target.value)}
+							/>
 						</div>
 					</div>
 					<button type="submit">Salvar</button>
